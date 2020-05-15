310ecbf9ca38adf3bb33b837ebe1e353f86fcb19
creepyjosuke67 <creepyjosuke67@gmail.com>
2020-05-15 12:06:32 -0400

    fixed app.js to have all files

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Home from './src/screens/Login';
 import NewUser from './src/screens/NewUser';
 import List from './src/screens/List';
+import FinalScreen from '.src/screens/FinalScreen';
 
 const AppNavigator = createStackNavigator(
 {
   Home,
   NewUser,
-  List
+  List, 
+  FinalScreen,
 },
 {
   initialRouteName:'Home'
